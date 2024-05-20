982d5ed1a3dc312a1fc9a358f53e43b22351c783
ElisabettaMirti <belmirti@gmail.com>
2024-05-20 18:10:27 +0200

    inserite bombe che si accendono insieme quando ne viene cliccata una (GRAZIE JACOPO E MARCO)

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const gridEl = document.querySelector('section.grid'); //Variabile per il contai
 
 const playButton = document.querySelector('button'); //Variabile per bottone play
 
+
+
 playButton.addEventListener('click', function() {
     newGame(100, gridEl);
 })
@@ -12,13 +14,30 @@ playButton.addEventListener('click', function() {
 
 
 function newGame (cellsNumber, containerElement) {
-    containerElement.innerHTML = ""
+    containerElement.innerHTML = ""; //svuoto la griglia
+    let punteggio = 0;  //variabile per il punteggio
+    let bombsArray = getBombs(cellsNumber, 16); //chiamo la funzione per generare bombe
+    console.log(bombsArray);
     for (let i = 0; i < cellsNumber; i++){
         const squareEl = document.createElement('article');
         squareEl.classList.add('square');
         squareEl.append(i + 1);
+        
+
+
         squareEl.addEventListener('click', function(){
-            squareEl.classList.add('active');
+            if (bombsArray.includes(i + 1)){
+                squareEl.classList.add('bomb');
+                let celle = document.getElementsByClassName('square');
+                console.log(celle, 'celle')
+                for (let i = 0; i < bombsArray.length; i++){
+                    let num = bombsArray[i] - 1;
+                    celle[num].classList.add('bomb')
+                    console.log(num, 'num');
+                }
+            } else {
+                squareEl.classList.add('safe');
+            }            
             console.log(squareEl.innerText)
         });
 
@@ -30,14 +49,15 @@ function newGame (cellsNumber, containerElement) {
 
 function getBombs (cellsNumber, bombsNumber) { //Funzione per creare la posizione casuale delle bombe
     const bombsArray = [];
-    while (bombPosition.lenght < bombsNumber) { 
-    let bombPosition = Math.floor(Math.random() * cellsNumber + 1);
+    while (bombsArray.length < bombsNumber) { 
+        let bombPosition = Math.floor(Math.random() * cellsNumber + 1);
         if (!bombsArray.includes(bombPosition)) {
             bombsArray.push(bombPosition);
         }
     }
-    console.log(bombsArray);
-    // return bombsArray;
+    return bombsArray;
 }
+
+
 
 
